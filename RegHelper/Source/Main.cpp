b7d0b2ed9c2ffc041cf9e2e95e647d13509b4f59
chris.hoen <chris.hoen@verizon.net>
2014-12-06 23:33:22 -0500

    initial

---
+++ RegHelper/Source/Main.cpp
@@ -0,0 +1,44 @@
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "prnPrint.h"
+#include "risCmdLineConsole.h"
+#include "CmdLineExec.h"
+
+void amain_init();
+
+
+//******************************************************************************
+int main(int argc,char** argv)
+{
+   //--------------------------------------------------------------------
+   // Begin program
+
+   amain_init();
+
+   Prn::print(0,0,"amain***************************************************BEGIN");
+   
+   //--------------------------------------------------------------------
+   // Start user command line executive,
+   // Wait for user to exit
+
+   CmdLineExec* exec = new CmdLineExec;
+   Ris::gCmdLineConsole.execute(exec);
+   delete exec;
+
+   //--------------------------------------------------------------------
+   // End program
+
+   Prn::print(0,0,"amain*****************************************************END");
+   return 0;
+}
+//******************************************************************************
+void amain_init()
+{
+   // Initialize print filters
+   Prn::initializePrint();
+
+   Ris::setConsoleTitle("test");
+}
+

117ba8ae8f80c6cc6c6ff4528dace896bb551f6d
chris.hoen <chris.hoen@verizon.net>
2016-01-28 15:37:31 -0500

    added risStatistics

---
+++ Statistics/Source/CmdLineExec.cpp
@@ -0,0 +1,227 @@
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "prnPrint.h"
+#include "my_functions.h"
+#include "risStatistics.h"
+
+#include "CmdLineExec.h"
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+   mStepFlag=true;
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"    ))  executeGo1  (aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2  (aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3  (aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4  (aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5  (aCmd);
+   if(aCmd->isCmd("GO6"    ))  executeGo6  (aCmd);
+   if(aCmd->isCmd("GO7"    ))  executeGo7  (aCmd);
+   if(aCmd->isCmd("GO8"    ))  executeGo8  (aCmd);
+   if(aCmd->isCmd("GO9"    ))  executeGo9  (aCmd);
+   if(aCmd->isCmd("GO10"   ))  executeGo10 (aCmd);
+   if(aCmd->isCmd("GO11"   ))  executeGo11 (aCmd);
+   if(aCmd->isCmd("GO12"   ))  executeGo12 (aCmd);
+   if(aCmd->isCmd("GO13"   ))  executeGo13 (aCmd);
+
+   if(aCmd->isCmd("STEP"   ))  mStepFlag=true;
+   if(aCmd->isCmd("RAMP"   ))  mStepFlag=false;
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo7(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo8(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo9(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo10(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo11(Ris::CmdLineCmd* aCmd)
+{
+   Ris::PeriodicStatistics tF;
+   tF.initialize(40);
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo12(Ris::CmdLineCmd* aCmd)
+{
+   Ris::PeriodicStatistics tF;
+   tF.initialize(40);
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(1.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo13(Ris::CmdLineCmd* aCmd)
+{
+   Ris::PeriodicStatistics tF;
+   tF.initialize(40);
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(2.0 + 10.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+   for (int i = 0; i<20; i++)
+   {
+      tF.put(-2.0 + 10.0);
+      tF.show();
+   }
+   Prn::print(0,"");
+
+
+}
+
---
+++ Statistics/Source/risStatistics.h
@@ -0,0 +1,67 @@
+#ifndef _RISSTATISTICS_H_
+#define _RISSTATISTICS_H_
+/*==============================================================================
+
+mean, stdev,min,max
+==============================================================================*/
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+namespace Ris
+{
+   //******************************************************************************
+   //******************************************************************************
+   //******************************************************************************
+   // This filter characterizes an input signal by calculating expectations and
+   // uncertainties of the signal. It does this on a periodic basis, given a fixed
+   // window size. For example, given a fixed window size of 1000, and an update
+   // rate of 1000 khz, this will produce new statisics once a second.
+
+   class PeriodicStatistics
+   {
+   public:
+      // Initialize
+      void initialize(int aSize);
+      void show();
+
+      // Put input value
+      void put(float aX);
+
+      // Members
+      int mSize;
+      bool mFirstFlag;
+
+      // Output values
+      bool   mEndOfPeriod;  // True at the end of a period
+      float  mX;            // Input value
+      float  mEX;           // expectation (mean)
+      float  mUX;           // uncertainty (standard deviation)
+      float  mMinX;         // minimum 
+      float  mMaxX;         // maximum
+      float  mMean;         // expectation (mean)
+      float  mStdDev;       // uncertainty (standard deviation)
+
+
+      float  mEXSquare;     // expectation of X squared
+      float  mVariance;     // variance
+
+      // Sums for expectation and uncertainty
+      float  mXSum;         // Sum of X
+      float  mXSquareSum;   // Sum of X squared
+      int    mPutCount;     // Put count
+
+      // Current min and max
+      float  mCurrentMinX;  // minimum
+      float  mCurrentMaxX;  // maximum
+
+
+      // Members
+      int mK;
+   };
+
+}//namespace
+#endif
+
+

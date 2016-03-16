70528ae1f677d7bd21219a2743dfefb8a68f64a6
chris.hoen <chris.hoen@verizon.net>
2016-03-16 11:29:54 -0400

    risTextFile

---
+++ FileTest/Source/CmdLineExec.cpp
@@ -0,0 +1,92 @@
+#include <stdarg.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "my_functions.h"
+#include "prnPrint.h"
+#include "risTextFile.h"
+
+#include "CmdLineExec.h"
+
+using namespace std;
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"  ))  reset();
+   if(aCmd->isCmd("GO1"    ))  executeGo1(aCmd);
+   if(aCmd->isCmd("GO2"    ))  executeGo2(aCmd);
+   if(aCmd->isCmd("GO3"    ))  executeGo3(aCmd);
+   if(aCmd->isCmd("GO4"    ))  executeGo4(aCmd);
+   if(aCmd->isCmd("GO5"    ))  executeGo5(aCmd);
+}
+
+
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   Ris::LogFileWriter tWriter;
+   tWriter.open("C:\\MyLib\\Data\\log_file_101.txt");
+   tWriter.write("%d %d",101,102);
+   tWriter.write("%d %d",201,202);
+   tWriter.close();
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+class MyClass1
+{
+public:
+
+   MyClass1()
+   {
+      mX1[0] = 101;
+      mX2[0] = 102;
+   }
+
+   enum {MaxSize = 100};
+   int  mX1[MaxSize];
+
+   static const int cMaxSize = 100;
+   int  mX2[cMaxSize];
+
+   void myCall(int aX)
+   {
+      const int* tPtr1 = &cMaxSize;
+      int* tPtr2 = (int*)&cMaxSize;
+   }
+
+};
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+

4c1103a7ff48c7b15e4f5119830f102a0af4515b
chris.hoen <chris.hoen@verizon.net>
2016-04-08 13:48:02 -0400

    cleanup

--- RisLib/Source/my_functions.cpp
+++ RisLib/Source/my_functions.cpp
@@ -386,7 +386,6 @@ char* my_stringLLU(char* aString,unsigned long long aValue)
    tString4[23]=0;
 
    sprintf(tString1, "%llu",aValue);
-   int tIndex[18] = {0,1,2, 4,5,6, 8,9,10 ,12,13,14 ,16,17,18, 20,21,22};
 
    int tLen1=(int)strlen(tString1);
    int tLen2=0;

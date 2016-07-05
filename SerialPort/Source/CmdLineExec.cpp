684f3f07b825902e2ad338e92c9b5e87e2c2152f
chrishoen <chris.hoen@red-inc.us>
2016-07-05 14:56:17 -0400

    added project SerialPort

---
+++ SerialPort/Source/CmdLineExec.cpp
@@ -0,0 +1,45 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "prnPrint.h"
+
+#include "CmdLineExec.h"
+
+#include "risPortableCalls.h"
+
+//******************************************************************************
+CmdLineExec::CmdLineExec()
+{
+}
+//******************************************************************************
+void CmdLineExec::reset()
+{
+}
+//******************************************************************************
+
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("GO1"   ))  executeGo1     (aCmd);
+   if(aCmd->isCmd("GO2"   ))  executeGo2     (aCmd);
+   if(aCmd->isCmd("GO3"   ))  executeGo3     (aCmd);
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
---
+++ SerialPort/Source/Main.cpp
@@ -0,0 +1,31 @@
+#include "prnPrint.h"
+#include "risThreadsProcess.h"
+#include "risCmdLineConsole.h"
+#include "CmdLineExec.h"
+
+#include "MainInit.h"
+
+//******************************************************************************
+int main(int argc,char** argv)
+{
+   //--------------------------------------------------------------------
+   // Initialize
+
+   main_initialize(argc,argv);
+
+   //--------------------------------------------------------------------
+   // Start user command line executive,
+   // It returns when user exits
+
+   CmdLineExec* tExec = new CmdLineExec;
+   Ris::executeCmdLineConsole(tExec);
+   delete tExec;
+
+   //--------------------------------------------------------------------
+   // Exit
+   
+   main_finalize();
+
+   return 0;
+}
+

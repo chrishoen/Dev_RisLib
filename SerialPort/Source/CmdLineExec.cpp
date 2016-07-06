78cb9d81506af0b1586363b84a9ab8da6e097666
chrishoen <chris.hoen@red-inc.us>
2016-07-06 09:30:30 -0400

    more

--- SerialPort/Source/CmdLineExec.cpp
+++ SerialPort/Source/CmdLineExec.cpp
@@ -23,6 +23,9 @@ void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
    if(aCmd->isCmd("GO1"   ))  executeGo1     (aCmd);
    if(aCmd->isCmd("GO2"   ))  executeGo2     (aCmd);
    if(aCmd->isCmd("GO3"   ))  executeGo3     (aCmd);
+
+   if(aCmd->isCmd("Open"  ))  executeOpen     (aCmd);
+   if(aCmd->isCmd("Close" ))  executeClose    (aCmd);
 }
 
 //******************************************************************************
@@ -41,5 +44,19 @@ void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
 {
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeOpen(Ris::CmdLineCmd* aCmd)
+{
+   mPort.doOpen(7,"38400,N,8,1",2000);
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeClose(Ris::CmdLineCmd* aCmd)
+{
+   mPort.doClose();
 }
 
--- SerialPort/Source/CmdLineExec.h
+++ SerialPort/Source/CmdLineExec.h
@@ -17,9 +17,12 @@ public:
   void reset();
   void execute(Ris::CmdLineCmd* aCmd);
 
-  void executeGo1     (Ris::CmdLineCmd* aCmd);
-  void executeGo2     (Ris::CmdLineCmd* aCmd);
-  void executeGo3     (Ris::CmdLineCmd* aCmd);
+  void executeGo1       (Ris::CmdLineCmd* aCmd);
+  void executeGo2       (Ris::CmdLineCmd* aCmd);
+  void executeGo3       (Ris::CmdLineCmd* aCmd);
+  void executeOpen      (Ris::CmdLineCmd* aCmd);
+  void executeClose     (Ris::CmdLineCmd* aCmd);
+
 
 
   Ris::SerialPort mPort;

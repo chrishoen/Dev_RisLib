18d02754cef4745586c87777c80ff216a929a714
chrishoen <chris.hoen@red-inc.us>
2017-06-23 21:19:11 -0400

    comments

--- RisLib/Source/risThreadsTwoThread.h
+++ RisLib/Source/risThreadsTwoThread.h
@@ -1,17 +1,24 @@
-#ifndef _RISTHREADSTWOTHREAD_H_
-#define _RISTHREADSTWOTHREAD_H_
+#pragma once
 
 /*==============================================================================
-
- 
+Two Thread, a thread construct that has a short term thread and a long term
+thread. The short term thread executes qcalls that have a short duration
+and the long term thread executes qcalls that can have long durations. Many
+short term qcalls can execute while one long term qcall executes. It can be
+used as a thread that can send a command somewhere and wait for a response.
 ==============================================================================*/
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+
 #include <atomic>
 #include "risLogic.h"
 #include "risThreadsQCallThread.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 
 namespace Ris
 {
@@ -30,43 +37,12 @@ namespace Threads
 class TwoThreadShortThread : public Ris::Threads::BaseQCallThread
 {
 public:
-   TwoThreadShortThread();
-  ~TwoThreadShortThread();
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Constants.
 
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   // Thread base class overloads, these call the call pointers listed below, 
-   // if they have been bound. The timer function also manages timer completion
-   // notification logic.
-
-   void threadInitFunction(); 
-   void threadExitFunction(); 
-   void threadExceptionFunction(char* aStr);
-   void executeOnTimer(int aTimerCount);
-
-   //---------------------------------------------------------------------------
-   // Associated thread call pointers, these are called by the
-   // above thread base overloads, if they are bound. They are
-   // bound to functions by the instantiator before the thread
-   // is launched. Any that are not bound result in a no op
-   // for the thread run function
-
-   CallPointer0<>       mThreadInitCallPointer;
-   CallPointer0<>       mThreadExitCallPointer;
-   CallPointer1<char*>  mThreadExceptionCallPointer;
-   CallPointer1<int>    mThreadExecuteOnTimerCallPointer;
-
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   // Timer completion notification.
-   // The above timer execution method posts to this semaphore,
-   // if the down counter is not zero and then decrements to zero.
-   BinarySemaphore  mTimerCompletionSem;
-   std::atomic<int> mTimerCompletionDownCounter;
-
-   // The following method returns a status code
+   // Returned status codes.
    enum
    {
       TimerCompletion_None        = 0,
@@ -76,22 +52,79 @@ public:
       TimerCompletion_ForcedError = 4,
    };
 
-   // This waits for timer completions
-   int threadWaitForTimerCompletion(
-      int aTimerCount); 
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members.
+
+   // Thread call pointers, these are called by the thread base overloads, if 
+   // they are bound. They are bound to functions by the instantiator before
+   // the thread is launched. Any that are not bound result in a no op for the
+   // thread run function.
+   CallPointer0<>       mThreadInitCallPointer;
+   CallPointer0<>       mThreadExitCallPointer;
+   CallPointer1<char*>  mThreadExceptionCallPointer;
+   CallPointer1<int>    mThreadExecuteOnTimerCallPointer;
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members.
+
+   // Timer completion notification.
+   // The above timer execution method posts to this semaphore,
+   // if the down counter is not zero and then decrements to zero.
+   BinarySemaphore  mTimerCompletionSem;
+   std::atomic<int> mTimerCompletionDownCounter;
 
    // Timer completion code, set by above method
    int mTimerCompletionCode;
 
-   // This aborts waits for timer completions
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Infastructure.
+
+   // Constructor.
+   TwoThreadShortThread();
+  ~TwoThreadShortThread();
+
+   // Thread init function. This is called by the base class immedidately 
+   // after the thread starts running.
+   // It calls the associated call pointer, if it exists.
+   void threadInitFunction() override;
+
+   // Thread exit function. This is called by the base class immedidately
+   //  before the thread is terminated.
+   // It calls the associated call pointer, if it exists.
+   void threadExitFunction() override;
+
+   // Thread exception function. This is called by the base class upon the
+   // occurence of an execption.
+   // It calls the associated call pointer, if it exists.
+   void threadExceptionFunction(char* aStr) override;
+
+   // Execute periodically. This is called by the base class timer.
+   // It calls the associated call pointer, if it exists.
+   void executeOnTimer(int aTimerCount) override;
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Waits for a timer completion.
+   int threadWaitForTimerCompletion(
+      int aTimerCount); 
+
+   // Abort any pending waits for timer completion.
    void threadAbortTimerCompletion();
 
-   // This forces a timer completion
+   // Forces a timer completion.
    void threadForceTimerCompletion();
 
-   // This forces a timer completion with an error
+   // Forces a timer completion with an error.
    void threadForceTimerCompletionWithError();
-
 };
 
 //******************************************************************************
@@ -121,36 +154,19 @@ class BaseTwoThread
 {
 public:
 
-   //--------------------------------------------------------------
-   // Constructor
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members.
 
-   BaseTwoThread();
-   virtual ~BaseTwoThread();
-
-   //--------------------------------------------------------------
-   // Thread methods
-
-   virtual void launchThreads(); 
-   virtual void shutdownThreads();
-
-   //--------------------------------------------------------------
-   // Thread methods, overloaded by inheritor
-   // These execute in the context of the short thread
-
-   virtual void threadInitFunction(){}
-   virtual void threadExitFunction(){}
-   virtual void threadExceptionFunction(char* aStr){}
-   virtual void executeOnTimer(int aTimerCount){}
-
-   //--------------------------------------------------------------
-   // Thread members
-
-   // Threads 
+   // The two threads. 
    TwoThreadShortThread*  mShortThread;
    TwoThreadLongThread*   mLongThread;
 
-   //--------------------------------------------------------------
-   // Exception members
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members.
 
    // If this is nonzero, then it is used to throw an
    // exception when a timer completion abort is caused.
@@ -164,64 +180,110 @@ public:
    // exception when an error is returned in a notification.
    int mTimerCompletionErrorException;
 
-   //--------------------------------------------------------------
-   // Wait for timer
-
-   // Long thread wait for timer.
-   // This waits for a timer completion.
-   // If the wait is aborted then it can throw an exception.
-   // It returns a TimerCompletion code.
-   int waitForTimer(int aTimeout = -1);
-
-   //--------------------------------------------------------------
-   // Long thread wait for a notification from short thread.
-   // This waits for a timer completion.
-   // If the wait times out  then it can throw an exception.
-   // If the wait is aborted then it can throw an exception.
-   // It returns a TimerCompletion code.
-   int waitForNotify(int aTimeout = -1);
-
-   //--------------------------------------------------------------
-   // Multiple notifications
-
-   // Long thread wait for a notification from short thread.
-   // One notification in the list.
-   void waitForNotify(int aTimeout, int aIndex);
-
-   // Long thread wait for a notification from short thread.
-   // Any notifications in the list.
-   void waitForNotifyAny(int aTimeout, int aNumArgs, ...);
-
-   // Long thread wait for a notification from short thread.
-   // All notifications in the list.
-   void waitForNotifyAll(int aTimeout, int aNumArgs, ...);
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Members.
 
    // Notification latch
    Ris::Logic::AndOrLatch mNotifyLatch;
-
-   void resetNotify(){ mNotifyLatch.reset(); }
 
    // This is used by the above waits.
    // If true  then waiting for any notification.
    // If false then waiting for all notifications.
    bool mWaitingForNotifyAny;
 
-   //--------------------------------------------------------------
-   // Short thread notifies long thread
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Infastucture.
 
-   // Short thread notifies long thread with a latch index
+   // Constructor
+   BaseTwoThread();
+   virtual ~BaseTwoThread();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Launch, shutdown the two threads.
+   void launchThreads(); 
+   void shutdownThreads();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods. These are overloaded by inheritors. They execute in the context
+   // of the short thread. They are called by the associated short term thread
+   // functions.
+
+   virtual void threadInitFunction(){}
+   virtual void threadExitFunction(){}
+   virtual void threadExceptionFunction(char* aStr){}
+   virtual void executeOnTimer(int aTimerCount){}
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Reset the notification latch.
+   void resetNotify();
+
+   // Wait for the timer.
+   // This is called from the long thread.
+   // This waits for a timer completion.
+   // If the wait is aborted then it can throw an exception.
+   // It returns a TimerCompletion code.
+   int waitForTimer(int aTimeout = -1);
+
+   // Wait for a notification from short thread.
+   // This is called from the long thread.
+   // This waits for a timer completion.
+   // If the wait times out  then it can throw an exception.
+   // If the wait is aborted then it can throw an exception.
+   // It returns a TimerCompletion code.
+   int waitForNotify(int aTimeout = -1);
+
+   // Wait for a notification from short thread.
+   // This is called from the long thread.
+   // One notification in the list.
+   void waitForNotify(int aTimeout, int aIndex);
+
+   // Wait for a notification from short thread.
+   // This is called from the long thread.
+   // Any notifications in the list.
+   void waitForNotifyAny(int aTimeout, int aNumArgs, ...);
+
+   // Wait for a notification from short thread.
+   // This is called from the long thread.
+   // All notifications in the list.
+   void waitForNotifyAll(int aTimeout, int aNumArgs, ...);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Notify the long thread with a latch index.
+   // This is called from the short thread.
    // This forces a timer completion, waking up the above waits.
    void notify(int aIndex);
 
-   // Abort long thread from waiting for notification from short thread.
+   // Abort the long thread from waiting for notification from short thread.
+   // This is called from the short thread.
    // This aborts the wait for a timer completion.
    void abortWaitForNotify();
 
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   //---------------------------------------------------------------------------
-   // Send notification qcall. This executes in the context of the short term
-   // thread to call notify(aIndex), which then notifies the long term thread.
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
+
+   // Send notification qcall.
+   // This is executed by the short thread to call notify(aIndex), which then
+   // notifies the long term thread.
 
    Ris::Threads::QCall3<int,int,void*> mSendNotifyQCall;
    void executeSendNotify(int aIndex,int aStatus,void* aData);
@@ -242,42 +304,55 @@ public:
 class TwoThreadNotify
 {
 public:
-   //---------------------------------------------------------------------------
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
    // Members.
 
+   // The two thread for which the notifications is intended.
    BaseTwoThread* mTwoThread;
-   int            mIndex;
-   int            mStatus;
-   void*          mData;
 
-   //---------------------------------------------------------------------------
-   // Constructors.
+   // Notification index.
+   int mIndex;
 
+   // Notification status. Zero means no error.
+   int mStatus;
+
+   // Generic data pointer that can be used to return some data with the 
+   // notification.
+   void* mData;
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Infastructure.
+
+   // Constructor.
    TwoThreadNotify();
    TwoThreadNotify(BaseTwoThread* aTwoThread,int aIndex);
 
-   //---------------------------------------------------------------------------
-   // This sets the status to a nonzero value. This will cause a notification 
-   // error exception on the thread waiting for the notification.
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Methods.
 
+   // Set the status to a nonzero value. This will cause a notification 
+   // error exception on the thread waiting for the notification.
    void setError();
 
-   //---------------------------------------------------------------------------
-   // This sets the data to a nonzero value. This will pass the data to  
+   // Sets the data to a nonzero value. This will pass the data to  
    // the thread waiting for the notification.
-
    void setData(void* aData);
 
-   //---------------------------------------------------------------------------
    // Send notify. This invokes the two thread qcall to execute in the 
-   // context of the short termm thread to notify the long term thread.
-
+   // context of the short term thread to notify the long term thread.
    void notify();
 
 };
 //******************************************************************************
+//******************************************************************************
+//******************************************************************************
 }//namespace
 }//namespace
 
-#endif
 

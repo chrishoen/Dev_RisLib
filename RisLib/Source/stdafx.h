82de0e0906b399743dac82ce7c12bda292266e7c
chrishoen <chris123hoen@gmail.com>
2018-08-29 09:43:58 -0400

    cleanup

--- RisLib/Source/stdafx.h
+++ RisLib/Source/stdafx.h
@@ -6,8 +6,6 @@
 #pragma once
 
 
-//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-//#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>

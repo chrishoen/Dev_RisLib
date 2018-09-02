e614596f4c7ca2ef1162a11eb48e9cb43fff21c3
chrishoen <chris123hoen@gmail.com>
2018-09-02 12:50:40 -0400

    removed logFiles timestamp

--- RisLib/Source/logFiles.cpp
+++ RisLib/Source/logFiles.cpp
@@ -8,7 +8,6 @@
 #include <stdarg.h>
 
 #include "risPortableCalls.h"
-#include "risSystemTime.h"
 #include "my_functions.h"
 #include "risAlphaDir.h"
 #include "logFiles.h"
@@ -115,20 +114,6 @@ void write (int aLogNum, const char* aFormat, ...)
    }
 
    //-----------------------------------------------------
-   // Get system time string.
-
-   char  tTimeString[40];
-   tTimeString[0]=0;
-
-   if (mTimestampEnable[aLogNum])
-   {
-      Ris::getCurrentSystemTimeAsString1(
-         tTimeString,
-         0,
-         " >> ");
-   }
-
-   //-----------------------------------------------------
    // Do a vsprintf with variable arg list into print string
 
    char tPrintString[cMaxStringSize];
@@ -146,29 +131,8 @@ void write (int aLogNum, const char* aFormat, ...)
    // Print the string
 
    char tOutputString[cMaxStringSize];
-   strcpy(tOutputString,tTimeString);
    strcat(tOutputString,tPrintString);
    fputs(tOutputString,mFile[aLogNum]);
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Write to log file
-
-void writeTimeStamp(int aLogNum, const char* aLabel)
-{
-   char tBuffer[40];
-   write(aLogNum,"%s $$ %s",aLabel,my_timestamp(tBuffer));
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// 
-void enableTimeStamp(int aLogNum, bool aEnableFlag)
-{
-   mTimestampEnable [aLogNum] = aEnableFlag;
 }
 
 //******************************************************************************

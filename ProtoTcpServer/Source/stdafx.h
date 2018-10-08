679c0864064b18e411ffd427d7e384503aeb1313
chrishoen <chris123hoen@gmail.com>
2018-10-08 15:09:39 -0400

    remove windows.h from stdafx.h

--- ProtoTcpServer/Source/stdafx.h
+++ ProtoTcpServer/Source/stdafx.h
@@ -1,13 +1,4 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
-
 #pragma once
-
-
-//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-#include <windows.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
@@ -16,8 +7,3 @@
 #include "prnPrint.h"
 #include "my_functions.h"
 #include "risPortableCalls.h"
-
-
-
-
-// TODO: reference additional headers your program requires here

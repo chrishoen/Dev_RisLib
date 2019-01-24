e2c57c15782b89be368a4fabc4e16ce9a474a7c9
chrishoen <chris123hoen@gmail.com>
2019-01-23 20:25:49 -0500

    RisLibConfig.cmake

--- RisLib/RisLibConfig.cmake
+++ RisLib/RisLibConfig.cmake
@@ -1,7 +1,7 @@
 ﻿if (MSVC)
    add_library(RisLib STATIC IMPORTED)
-   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION "C:\MyTools\MyLib\lib\RisLib.lib")
-   target_include_directories(RisLib INTERFACE "C:\MyTools\MyLib\include\RisLib")
+   set_target_properties(RisLib PROPERTIES IMPORTED_LOCATION "C:\\MyTools\\MyLib\\lib\\RisLib.lib")
+   target_include_directories(RisLib INTERFACE "C:\\MyTools\\MyLib\\include\\RisLib")
 
    target_link_libraries(RisLib INTERFACE ws2_32)
    target_link_libraries(RisLib INTERFACE winmm)

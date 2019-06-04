5029352a592e4f558489c6173bc7eb010189b2f6
chrishoen <chris123hoen@gmail.com>
2019-06-04 11:27:00 -0400

    added risHiresTime

---
+++ RisLib/risHiresTime_linux.cpp
@@ -0,0 +1,33 @@
+/*==============================================================================
+==============================================================================*/
+#include "stdafx.h"
+
+#include <time.h>
+#include <sys/time.h>
+
+#include "risHiresTime.h"
+
+namespace Ris
+{
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Return the current high resolution timer value in nanoseconds. 
+
+unsigned long long int getCurrentHiresTime()
+{
+   struct timespec tTimespec;
+   clock_gettime(CLOCK_MONOTONIC, &tTimespec);
+
+   unsigned long long int tSeconds = (unsigned long long int)tTimespec.tv_sec;
+   unsigned long long int tNanoseconds = (unsigned long long int)tTimespec.tv_nsec;
+   unsigned long long int tTimeNs = tSeconds * 1000 * 1000 * 1000 + tNanoseconds;
+   return tTimeNs;
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+}//namespace
+

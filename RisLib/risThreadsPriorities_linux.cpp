a088460c750b750bb5cf86da38f9c9c989701fb2
chrishoen <chris123hoen@gmail.com>
2019-06-24 11:57:25 -0400

    timer thread test thread priorities

--- RisLib/risThreadsPriorities_linux.cpp
+++ RisLib/risThreadsPriorities_linux.cpp
@@ -53,7 +53,7 @@ Priorities::Priorities()
    mTcpClient = Priority(-1, 50);
    mSerial    = Priority(-1, 80);
    mTimerTest = Priority( 3, 95);
-   mTimerTest = Priority( 2, 95);
+   mTimerTest = Priority(-1, 95);
 
    mTsPrint   = Priority(-1, 20);
 }

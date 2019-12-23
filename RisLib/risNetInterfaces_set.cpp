718c0851fce4ca9ed2b7ae5c4bccb24727e5b7d7
chrishoen <chris123hoen@gmail.com>
2019-12-23 13:31:54 -0500

    static ip address remplate string replacements work

--- RisLib/risNetInterfaces_set.cpp
+++ RisLib/risNetInterfaces_set.cpp
@@ -46,13 +46,48 @@ void Interfaces::doSetNetSettingsEth0_dhcp()
 
 void Interfaces::doSetNetSettingsEth0_static()
 {
-   // Read the json value from the input file.
+   // Input and output files.
    std::ifstream tInputFile("/opt/prime/special/interfaces_static");
    std::ofstream tOutputFile("/opt/prime/special/tmp_interfaces_static");
-   std::string tLine;
-   while (std::getline(tInputFile, tLine))
+
+   // String variables.
+   std::string tInputLine;
+   std::string tOutputLine;
+   size_t tFound = 0;
+   std::string tReplaceAddress = "REPLACE_ADDRESS";
+   std::string tReplaceMask = "REPLACE_MASK";
+   std::string tReplaceGateway = "REPLACE_GATEWAY";
+
+   // Copy the input template file to the output file. Find and replace
+   // tags for address, mask, and gateway.
+   while (std::getline(tInputFile, tInputLine))
    {
-      tOutputFile << tLine << std::endl;
+      // Copy input line to output line.
+      tOutputLine = tInputLine;
+
+      // Find and replace address tag. 
+      tFound = tOutputLine.find(tReplaceAddress);
+      if (tFound != std::string::npos)
+      {
+         tOutputLine.replace(tFound, tReplaceAddress.length(),mEth0Address);
+      }
+
+      // Find and replace mask tag. 
+      tFound = tOutputLine.find(tReplaceMask);
+      if (tFound != std::string::npos)
+      {
+         tOutputLine.replace(tFound, tReplaceMask.length(), mEth0Mask);
+      }
+
+      // Find and replace gateway tag. 
+      tFound = tOutputLine.find(tReplaceGateway);
+      if (tFound != std::string::npos)
+      {
+         tOutputLine.replace(tFound, tReplaceGateway.length(), mEth0Gateway);
+      }
+
+      // Write the the output line.
+      tOutputFile << tOutputLine << std::endl;
    }
    tInputFile.close();
    tOutputFile.close();

a0f9bf930c99d550e4c23001e41cadf73cd61215
chrishoen <chris123hoen@gmail.com>
2019-12-28 02:11:52 -0500

    windows version of thread synch

--- RisLib/risThreadsSynch_win.cpp
+++ RisLib/risThreadsSynch_win.cpp
@@ -24,45 +24,40 @@ namespace Threads
 //******************************************************************************
 //******************************************************************************
 
+// Specific implementation variables.
 class BinarySemaphore::Specific
 {
 public:
    HANDLE mHandle;
 };
 
-//******************************************************************************
-
-BinarySemaphore::BinarySemaphore() 
+// Constructor. Create the semaphore.
+BinarySemaphore::BinarySemaphore()
 {
    mSpecific = new Specific;
    mSpecific->mHandle = CreateEvent(NULL,FALSE,FALSE,NULL);
-   mStatusCode=0;
 }
 
-//******************************************************************************
-
-BinarySemaphore::~BinarySemaphore() 
-{
-   CloseHandle(mSpecific->mHandle);
-   delete mSpecific;
-}
-
-//******************************************************************************
-
+// Reset the semaphore.
 void BinarySemaphore::reset()
 {
    ResetEvent(mSpecific->mHandle);
 }
 
-//******************************************************************************
+// Destructor. Delete the semaphore.
+BinarySemaphore::~BinarySemaphore()
+{
+   CloseHandle(mSpecific->mHandle);
+   delete mSpecific;
+}
 
+// Put to the semaphore.
 void BinarySemaphore::put()
 {
    SetEvent(mSpecific->mHandle);
 }
 
-//******************************************************************************
-
+// Get from the semaphore, block until timeout, return true if no timeout.
 bool BinarySemaphore::get(int timeout)
 {
    return WaitForSingleObject(mSpecific->mHandle,timeout) != WAIT_TIMEOUT ;
@@ -71,44 +66,43 @@ bool BinarySemaphore::get(int timeout)
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+// Counting semaphore.
 
+// Specific implementation variables.
 class CountingSemaphore::Specific
 {
 public:
    HANDLE mHandle;
 };
 
-//******************************************************************************
-
-CountingSemaphore::CountingSemaphore() 
+// Constructor. Create the semaphore.
+CountingSemaphore::CountingSemaphore()
 {
    mSpecific = new Specific;
    mSpecific->mHandle = CreateSemaphore(NULL,0,1000000,NULL);
 }
 
-CountingSemaphore::CountingSemaphore(int aInitial) 
+// Constructor. Create the semaphore.
+CountingSemaphore::CountingSemaphore(int aInitial)
 {
    mSpecific = new Specific;
    mSpecific->mHandle = CreateSemaphore(NULL,aInitial,1000000,NULL);
 }
 
-//******************************************************************************
-
-CountingSemaphore::~CountingSemaphore() 
+// Destructor. Delete the semaphore.
+CountingSemaphore::~CountingSemaphore()
 {
    CloseHandle(mSpecific->mHandle);
    delete mSpecific;
 }
 
-//******************************************************************************
-
+// Put to the semaphore.
 void CountingSemaphore::put()
 {
    ReleaseSemaphore(mSpecific->mHandle,1,NULL);
 }
 
-//******************************************************************************
-
+// Get from the semaphore, block until timeout, return true if no timeout.
 bool CountingSemaphore::get(int timeout)
 {
    switch (WaitForSingleObject(mSpecific->mHandle,timeout))
@@ -122,150 +116,124 @@ bool CountingSemaphore::get(int timeout)
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+// Mutex semaphore.
 
+// Specific implementation variables.
 class MutexSemaphore::Specific
 {
 public:
    SRWLOCK mSRWLock;
 };
 
-//******************************************************************************
-
-MutexSemaphore::MutexSemaphore() 
+// Constructor. Create the mutex.
+MutexSemaphore::MutexSemaphore()
 {
    mSpecific = new Specific;
    InitializeSRWLock(&mSpecific->mSRWLock); 
 }
 
-//******************************************************************************
-
-MutexSemaphore::~MutexSemaphore() 
+// Destructor. Delete the mutex.
+MutexSemaphore::~MutexSemaphore()
 {
    delete mSpecific;
 }
 
-//******************************************************************************
-
+// Lock the mutex.
 void MutexSemaphore::lock()
 {
    AcquireSRWLockExclusive(&mSpecific->mSRWLock);
 }
 
-//******************************************************************************
-
+// Unlock the mutex.
 void MutexSemaphore::unlock()
 {
    ReleaseSRWLockExclusive(&mSpecific->mSRWLock); 
 }
 
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
 
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Named mutex.
+
+// Specific implementation variables.
+class NamedMutex::Specific
+{
+public:
+   SRWLOCK mSRWLock;
+};
+
+// Constructor. Create the mutex.
+NamedMutex::NamedMutex(const char* aName)
+{
+   mSpecific = new Specific;
+   initialize(aName);
+}
+
+// Constructor. default. Initialize the data structure.
+NamedMutex::NamedMutex()
+{
+   mSpecific = new Specific;
+}
+
+// Create the mutex. Call this if using default constructor.
+void NamedMutex::initialize(const char* aName)
+{
+   InitializeSRWLock(&mSpecific->mSRWLock);
+}
+
+// Destructor. Delete the mutex.
+NamedMutex::~NamedMutex()
+{
+   delete mSpecific;
+}
+
+// Lock the mutex.
+void NamedMutex::lock()
+{
+}
+
+// Unlock the mutex.
+void NamedMutex::unlock()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Critical section.
+
+// Specific implementation variables.
 class CriticalSection::Specific
 {
 public:
    CRITICAL_SECTION mCriticalSection; 
 };
 
-//******************************************************************************
-
-CriticalSection::CriticalSection() 
+// Constructor. Create the critical section.
+CriticalSection::CriticalSection()
 {
    mSpecific = new Specific;
    InitializeCriticalSectionAndSpinCount(&mSpecific->mCriticalSection,0x80000400); 
 }
 
-//******************************************************************************
-
-CriticalSection::~CriticalSection() 
+// Destructor. Delete the critical section.
+CriticalSection::~CriticalSection()
 {
    DeleteCriticalSection(&mSpecific->mCriticalSection);
    delete mSpecific;
 }
 
-//******************************************************************************
-
+// Enter the critical section.
 void CriticalSection::enter()
 {
    EnterCriticalSection(&mSpecific->mCriticalSection); 
 }
 
-//******************************************************************************
-
+// Leave the critical section.
 void CriticalSection::leave()
 {
    LeaveCriticalSection(&mSpecific->mCriticalSection);
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-
-class ConditionVariable::Specific
-{
-public:
-   SRWLOCK            mSRWLock; 
-   CONDITION_VARIABLE mConditionVariable; 
-   int                mPredicate;
-};
-
-//******************************************************************************
-
-ConditionVariable::ConditionVariable() 
-{
-   mSpecific = new Specific;
-   InitializeSRWLock(&mSpecific->mSRWLock); 
-   InitializeConditionVariable(&mSpecific->mConditionVariable); 
-   mSpecific->mPredicate = 0;
-}
-
-//******************************************************************************
-
-ConditionVariable::~ConditionVariable() 
-{
-   delete mSpecific;
-}
-
-//******************************************************************************
-
-void ConditionVariable::acquireLock()
-{
-   AcquireSRWLockExclusive(&mSpecific->mSRWLock); 
-}
-
-//******************************************************************************
-
-void ConditionVariable::releaseLock()
-{
-   ReleaseSRWLockExclusive(&mSpecific->mSRWLock); 
-}
-
-//******************************************************************************
-
-void ConditionVariable::waitFor()
-{
-   AcquireSRWLockExclusive(&mSpecific->mSRWLock); 
-
-   while (mSpecific->mPredicate == 0)
-   {
-      SleepConditionVariableSRW(&mSpecific->mConditionVariable, &mSpecific->mSRWLock, INFINITE, 0);
-   }
-   mSpecific->mPredicate = 0;
-
-   ReleaseSRWLockExclusive(&mSpecific->mSRWLock); 
-}
-
-//******************************************************************************
-
-void ConditionVariable::wakeUp(int aPredicate)
-{
-//    AcquireSRWLockExclusive(&mSpecific->mSRWLock); 
-   mSpecific->mPredicate = aPredicate;
-//    ReleaseSRWLockExclusive(&mSpecific->mSRWLock); 
-
-   WakeConditionVariable(&mSpecific->mConditionVariable);
-
 }
 
 //******************************************************************************

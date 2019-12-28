c7778a2f4bca7c65638cf8e36900c582c18f6fe5
chrishoen <chris123hoen@gmail.com>
2019-12-28 00:14:17 -0500

    comments

--- RisLib/risThreadsSynch.h
+++ RisLib/risThreadsSynch.h
@@ -187,7 +187,7 @@ public:
    //***************************************************************************
    // Members.
 
-   // Specific implementation variables. pimpl pattern.
+   // Specific implementation variables.
    class Specific;
    Specific* mSpecific;
 

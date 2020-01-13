365bf95976b8934c4c938ba96a2d5e9206fbbdce
chrishoen <chris123hoen@gmail.com>
2020-01-13 15:52:50 -0500

    ThreadTest works with libRisLib.so

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -36,36 +36,11 @@ function(my_lib_import_RisLib _target)
       target_link_libraries(${_target} PThreadLib)
       target_link_options(${_target} PRIVATE "-lrt")
    endif()
-
-   if (false)
-      set(THREADS_PREFER_PTHREAD_FLAG ON)
-      find_package(Threads REQUIRED)
-      target_link_libraries(${_target} Threads::Threads)
-   endif()
 endfunction()
 
 function(my_inc_import_RisLib _target)
 
    target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:RisLib,INTERFACE_INCLUDE_DIRECTORIES>)
-
-endfunction()
-
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-
-function(my_lib_import_RisLib22 _target)
-
-   target_link_libraries(${_target} RisLib)
-
-   if (MSVC)
-      target_link_libraries(${_target} ws2_32)
-      target_link_libraries(${_target} winmm)
-   else()
-      set(THREADS_PREFER_PTHREAD_FLAG ON)
-      find_package(Threads REQUIRED)
-      target_link_libraries(${_target} Threads::Threads)
-   endif()
 
 endfunction()
 

961a074d276edeec6c207b1470945674d26a76e5
chrishoen <chris123hoen@gmail.com>
2021-02-22 19:54:39 -0500

    back in business

--- RisLib/risProgramTime_linux.cpp
+++ RisLib/risProgramTime_linux.cpp
@@ -24,7 +24,10 @@ namespace Ris
 inline long long int my_get_hires_count()
 {
    struct timespec tTimespec;
-   clock_gettime(CLOCK_MONOTONIC, &tTimespec);
+// clock_gettime(CLOCK_BOOTTIME_ALARM, &tTimespec);
+
+//   clock_gettime(CLOCK_MONOTONIC_RAW, &tTimespec);
+   clock_gettime(CLOCK_REALTIME, &tTimespec);
 
    long long int tSeconds = (long long int)tTimespec.tv_sec;
    long long int tNanoseconds = (long long int)tTimespec.tv_nsec;

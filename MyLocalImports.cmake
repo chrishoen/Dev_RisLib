5b30293db2bea30329451050e7bec25aacf13e12
chrishoen <chris123hoen@gmail.com>
2021-11-12 08:07:23 -0500

    cmake linker stuff for pthread and rt libs

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -18,29 +18,6 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_RisLib22 _target)
-
-   if (MSVC)
-      target_link_libraries(${_target} RisLib)
-      target_link_libraries(${_target} ws2_32)
-      target_link_libraries(${_target} winmm)
-   else()
-      target_link_libraries(${_target} RisLib)
-   endif()
-   return()
-
-endfunction()
-
-function(my_inc_import_RisLib _target)
-
-   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:RisLib,INTERFACE_INCLUDE_DIRECTORIES>)
-
-endfunction()
-
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-
 function(my_lib_import_RisLib _target)
 
    if (MSVC)
@@ -48,34 +25,10 @@ function(my_lib_import_RisLib _target)
       target_link_libraries(${_target} ws2_32)
       target_link_libraries(${_target} winmm)
    else()
-      if (CMAKE_SYSTEM_VERSION EQUAL 101)
-         set (MyPThreadImportPath  "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/libpthread.so")
-         set (MyRTImportPath  "C:/Beagle/toolchain/arm-linux-gnueabihf/lib/librt.so")
-      else()
-         set (MyPThreadImportPath  "/lib64/libpthread.so")
-         set (MyRTImportPath  "/lib64/librt.so")
-         set (MyPThreadImportPath  "/usr/lib/x86_64-linux-gnu/libpthread.so")
-         set (MyRTImportPath  "/usr/lib/x86_64-linux-gnu/librt.so")
-      endif()
-      add_library(PThreadLib SHARED IMPORTED)
-      add_library(RTLib SHARED IMPORTED)
-      set_target_properties(PThreadLib PROPERTIES IMPORTED_LOCATION ${MyPThreadImportPath})
-      set_target_properties(RTLib PROPERTIES IMPORTED_LOCATION ${MyRTImportPath})
-
       target_link_libraries(${_target} RisLib)
-      target_link_libraries(${_target} PThreadLib)
-      target_link_libraries(${_target} RTLib)
+      target_link_libraries(${_target} pthread)
+      target_link_libraries(${_target} rt)
    endif()
-   return()
-
-   if (MSVC)
-      target_link_libraries(${_target} RisLib)
-      target_link_libraries(${_target} ws2_32)
-      target_link_libraries(${_target} winmm)
-   else()
-      target_link_libraries(${_target} RisLib)
-   endif()
-   return()
 
 endfunction()
 
@@ -84,4 +37,8 @@ function(my_inc_import_RisLib _target)
    target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:RisLib,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
+
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
 

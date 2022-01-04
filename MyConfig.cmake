0a7e3f05704a2a64d512fb3323a487d63dde53dd
chrishoen <chris123hoen@gmail.com>
2022-01-03 21:25:42 -0500

    new cmake works for windows

--- MyConfig.cmake
+++ MyConfig.cmake
@@ -9,7 +9,7 @@ function(my_init_config_variables)
 
    set (MyTargetHostName "127.0.0.1" PARENT_SCOPE)
 
-   set (MyCopyBinToDir "C:/aaa_prime/CMake" PARENT_SCOPE)
+   set (MyCopyBinToDir "C:/aaa_prime/RisLib" PARENT_SCOPE)
    set (MyCopyLibToDir "C:/MyTools/MyLib" PARENT_SCOPE)
 
    set (MySudo "" PARENT_SCOPE)

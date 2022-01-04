72d2c114d3fc6686e751b51c75d9ead37a11d9b1
chrishoen <chris123hoen@gmail.com>
2022-01-03 22:09:02 -0500

    new cmake for dart works, but not intellisense

--- MyConfig.cmake
+++ MyConfig.cmake
@@ -1,18 +1,19 @@
 #*******************************************************************************
 #*******************************************************************************
 #*******************************************************************************
-# copied from MyConfig_beagle.cmake
+# copied from MyConfig_dart.cmake
 
 function(my_init_config_variables)
 
-   set (MyTargetType beagle PARENT_SCOPE)
+   set (MyTargetType dart PARENT_SCOPE)
 
-   set (MyTargetHostName "debian@bbx" PARENT_SCOPE)
+   set (MyTargetHostName "root@dart" PARENT_SCOPE)
 
-   set (MyCopyBinToDir "/opt/prime/bin" PARENT_SCOPE)
-   set (MyCopyLibToDir "/opt/prime/lib" PARENT_SCOPE)
+   set (MyCopyBinToDir "/home/root/stenograph/bin" PARENT_SCOPE)
+   set (MyCopyLibToDir "/home/root/stenograph/lib" PARENT_SCOPE)
+   set (SRC_LINUX_DIR "/home/main/work/NextGen/src_linux"  PARENT_SCOPE)
 
-   set (MySudo "sudo " PARENT_SCOPE)
+   set (MySudo "" PARENT_SCOPE)
 
 endfunction()
 

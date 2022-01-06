101f5edebdd23bf48741d78b6ff2d2f63c42b39d
chrishoen <chris123hoen@gmail.com>
2022-01-05 20:24:58 -0500

    more readline

--- MyConfig.cmake
+++ MyConfig.cmake
@@ -1,19 +1,19 @@
 #*******************************************************************************
 #*******************************************************************************
 #*******************************************************************************
-# copied from MyConfig_ubuntu.cmake
+# copied from MyConfig_dart.cmake
 
 function(my_init_config_variables)
 
-   set (MyTargetType ubuntu PARENT_SCOPE)
+   set (MyTargetType dart PARENT_SCOPE)
 
-   set (MyTargetHostName "main@cpdev1" PARENT_SCOPE)
-   set (MyTargetHostName "127.0.0.1" PARENT_SCOPE)
+   set (MyTargetHostName "root@dart" PARENT_SCOPE)
 
-   set (MyCopyBinToDir "/opt/prime/bin" PARENT_SCOPE)
-   set (MyCopyLibToDir "/opt/prime/lib" PARENT_SCOPE)
+   set (MyCopyBinToDir "/home/root/stenograph/bin" PARENT_SCOPE)
+   set (MyCopyLibToDir "/home/root/stenograph/lib" PARENT_SCOPE)
+   set (SRC_LINUX_DIR "/home/main/work/NextGen/src_linux"  PARENT_SCOPE)
 
-   set (MySudo "sudo " PARENT_SCOPE)
+   set (MySudo "" PARENT_SCOPE)
 
 endfunction()
 

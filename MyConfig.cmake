056a1b4eaf11ed6a20d8aac18899a28458ac7648
chrishoen <chris123hoen@gmail.com>
2022-01-18 17:29:35 -0500

    new dart cmake stuff. rislib is static for dart

--- MyConfig.cmake
+++ MyConfig.cmake
@@ -1,19 +1,19 @@
 #*******************************************************************************
 #*******************************************************************************
 #*******************************************************************************
-# copied from MyConfig_beagle.cmake
+# copied from MyConfig_dart.cmake
 
 function(my_init_config_variables)
 
-   set (MyTargetType beagle PARENT_SCOPE)
+   set (MyTargetType dart PARENT_SCOPE)
 
-   set (MyTargetHostName "pi@rpx" PARENT_SCOPE)
-   set (MyTargetHostName "debian@bbkbd" PARENT_SCOPE)
-   
-   set (MyCopyBinToDir "/opt/prime/bin" PARENT_SCOPE)
-   set (MyCopyLibToDir "/opt/prime/lib" PARENT_SCOPE)
+   set (MyTargetHostName "root@dart" PARENT_SCOPE)
 
-   set (MySudo "sudo " PARENT_SCOPE)
+   set (MyCopyBinToDir "/home/root/stenograph/bin" PARENT_SCOPE)
+   set (MyCopyLibToDir "/home/root/stenograph/lib" PARENT_SCOPE)
+   set (SRC_LINUX_DIR "/home/main/work/nextgen/src_linux"  PARENT_SCOPE)
+
+   set (MySudo "" PARENT_SCOPE)
 
 endfunction()
 

ea869f87e5c2cd308a3de15cee2ef43c0bf89917
chrishoen <chris123hoen@gmail.com>
2022-01-23 10:05:11 -0500

    renamed toolchain file

--- MySpecial.cmake
+++ MySpecial.cmake
@@ -5,22 +5,13 @@
 function(my_special1)
 
    message(STATUS "my_special1***********************************************************BEGIN")
-   message(STATUS "CMAKE_SYSTEM                " ${CMAKE_SYSTEM})
-   message(STATUS "CMAKE_SYSTEM_VERSION        " ${CMAKE_SYSTEM_VERSION})
-   message(STATUS "CMAKE_BUILD_TYPE            " ${CMAKE_BUILD_TYPE})
-   message(STATUS "CMAKE_INSTALL_PREFIX        " ${CMAKE_INSTALL_PREFIX})
-   message(STATUS "DESTDIR                     " ${DESTDIR})
-   message(STATUS "PROJECT_SOURCE_DIR          " ${PROJECT_SOURCE_DIR})
-   message(STATUS "PROJECT_INSTALL_DIR         " ${PROJECT_INSTALL_DIR})
+   message(STATUS "MYMODE                 " ${MYMODE})
+   message(STATUS "CMAKE_SYSTEM           " ${CMAKE_SYSTEM})
+   message(STATUS "CMAKE_BUILD_TYPE       " ${CMAKE_BUILD_TYPE})
+   message(STATUS "CMAKE_INSTALL_PREFIX   " ${CMAKE_INSTALL_PREFIX})
+   message(STATUS "PROJECT_SOURCE_DIR     " ${PROJECT_SOURCE_DIR})
 
-   message(STATUS "MYMODE                       " ${MYMODE})
-   message(STATUS "MyTargetHostName            " ${MyTargetHostName})
-   message(STATUS "MyCopyBinToDir              " ${MyCopyBinToDir})
-   message(STATUS "MyCopyLibToDir              " ${MyCopyLibToDir})
-   message(STATUS "MySudo                      " ${MySudo})
-   message(STATUS "SRC_LINUX_DIR               " ${SRC_LINUX_DIR})
-
-   message(STATUS "Using toolchain file: ${CMAKE_TOOLCHAIN_FILE}")
+   message(STATUS "Using toolchain file:  " ${CMAKE_TOOLCHAIN_FILE})
    message(STATUS "my_special1***********************************************************END")
 endfunction()
 

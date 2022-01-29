fb280d38347d5576b369f5dd0ffcf95a0b96fe3f
chrishoen <chris123hoen@gmail.com>
2022-01-28 21:19:41 -0500

    added risBitUtils

--- Test/CmdLineExec.cpp
+++ Test/CmdLineExec.cpp
@@ -6,6 +6,7 @@
 #include "string.h"
 
 #include "risProgramTime.h"
+#include "risBitUtils.h"
 #include "my_functions.h"
 #include "CmdLineExec.h"
 
@@ -39,6 +40,7 @@ void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
    if (aCmd->isCmd("GO3"))    executeGo3(aCmd);
    if (aCmd->isCmd("GO4"))    executeGo4(aCmd);
    if (aCmd->isCmd("GO5"))    executeGo5(aCmd);
+   if (aCmd->isCmd("GO6"))    executeGo6(aCmd);
 }
 
 //******************************************************************************
@@ -47,8 +49,16 @@ void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
 {
-   Prn::print(0, "Ris::portableGetCurrentDir() %s", Ris::portableGetCurrentDir());
-   Prn::print(0, "Ris::portableGetProgramDir() %s", Ris::portableGetProgramDir());
+   aCmd->setArgDefault(1, 0);
+   aCmd->setArgDefault(2, true);
+   unsigned tValue = 0x0000ffff;
+   Prn::print(0, "%08x", tValue);
+
+   int tBitNum = aCmd->argInt(1);
+   bool tBitValue = aCmd->argBool(2);
+   Ris::setBit(&tValue, tBitNum, tBitValue);
+
+   Prn::print(0, "%08x", tValue);
 }
 
 //******************************************************************************
@@ -57,6 +67,16 @@ void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
 {
+   aCmd->setArgDefault(1, 0);
+   unsigned tValue = 0;
+   tValue = 0x00000000;
+   tValue = 0x0000ffff;
+   Prn::print(0, "%08x", tValue);
+
+   int tBitNum = aCmd->argInt(1);
+   bool tBitValue = Ris::getBit(tValue, tBitNum);
+
+   Prn::print(0, "%d", tBitValue);
 }
 
 //******************************************************************************
@@ -65,6 +85,19 @@ void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
 {
+   aCmd->setArgDefault(1, 0);
+   aCmd->setArgDefault(2, true);
+   unsigned tValue1 = 0;
+   unsigned tValue2 = 0;
+   tValue1 = 0x00000000;
+   tValue1 = 0x0000ffff;
+   Prn::print(0, "%08x", tValue1);
+
+   int tBitNum = aCmd->argInt(1);
+   bool tBitValue = aCmd->argBool(2);
+   tValue2 = Ris::setBit(tValue1, tBitNum, tBitValue);
+
+   Prn::print(0, "%08x", tValue2);
 }
 
 //******************************************************************************
@@ -73,11 +106,16 @@ void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
 
 void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
 {
-   double tTime = Ris::getProgramTime();
-   unsigned int tTimeMS = Ris::getCpuTimeUIntMS();
+   aCmd->setArgDefault(1, 0);
+   aCmd->setArgDefault(2, true);
+   unsigned char tValue = 0x0f;
+   Prn::print(0, "%02x", tValue);
 
-   Prn::print(0, "Ris::getProgramTime           %10.6f", tTime);
-   Prn::print(0, "Ris::getCpuTimeUIntMs         %10d", tTimeMS);
+   int tBitNum = aCmd->argInt(1);
+   bool tBitValue = aCmd->argBool(2);
+   Ris::setBit(&tValue, tBitNum, tBitValue);
+
+   Prn::print(0, "%02x", tValue);
 }
 
 //******************************************************************************
@@ -90,5 +128,20 @@ void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
    Prn::print(0, "Prn::print 0");
    Prn::print(Prn::Show1, "Prn::print Prn::Show1");
    Prn::print(Prn::Show2, "Prn::print Prn::Show");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
+{
+   Prn::print(0, "Ris::portableGetCurrentDir() %s", Ris::portableGetCurrentDir());
+   Prn::print(0, "Ris::portableGetProgramDir() %s", Ris::portableGetProgramDir());
+   double tTime = Ris::getProgramTime();
+   unsigned int tTimeMS = Ris::getCpuTimeUIntMS();
+
+   Prn::print(0, "Ris::getProgramTime           %10.6f", tTime);
+   Prn::print(0, "Ris::getCpuTimeUIntMs         %10d", tTimeMS);
 }
 
--- Test/CmdLineExec.h
+++ Test/CmdLineExec.h
@@ -58,6 +58,7 @@ public:
    void executeGo3(Ris::CmdLineCmd* aCmd);
    void executeGo4(Ris::CmdLineCmd* aCmd);
    void executeGo5(Ris::CmdLineCmd* aCmd);
+   void executeGo6(Ris::CmdLineCmd* aCmd);
 };
 
 //******************************************************************************

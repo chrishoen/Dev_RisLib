2aceed0462c37b95fd5e6d4c0d73a219960c912a
chrishoen <chris123hoen@gmail.com>
2022-03-08 21:39:35 -0500

    added RandomSleep

--- RisLib/risSleep_linux.cpp
+++ RisLib/risSleep_linux.cpp
@@ -48,5 +48,69 @@ void sleepUs(int aSleepForUs)
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
+
+RandomSleepUs::RandomSleepUs()
+{
+   // Set base class variables.
+   mSleepUs1 = 1000000;
+   mSleepUs2 = 1000000;
+}
+
+RandomSleepUs::RandomSleepUs(int aSleepUs1, int aSleepUs2)
+{
+   initialize(aSleepUs1, aSleepUs2);
+}
+
+void RandomSleepUs::initialize(int aSleepUs1, int aSleepUs2)
+{
+   // Seed random generator.
+   std::random_device tRandomDevice;
+   mRandomGenerator.seed(tRandomDevice());
+   mRandomDistribution = std::uniform_int_distribution<>(mSleepUs1, mSleepUs2);
+}
+
+void RandomSleepUs::doSleep()
+{
+   // Get a random sleep time.
+   int tSleepUs = mRandomDistribution(mRandomGenerator);
+   // Sleep.
+   sleepUs(tSleepUs);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+RandomSleepMs::RandomSleepMs()
+{
+   // Set base class variables.
+   mSleepMs1 = 1000;
+   mSleepMs2 = 1000;
+}
+
+RandomSleepMs::RandomSleepMs(int aSleepMs1, int aSleepMs2)
+{
+   initialize(aSleepMs1, aSleepMs2);
+}
+
+void RandomSleepMs::initialize(int aSleepMs1, int aSleepMs2)
+{
+   // Seed random generator.
+   std::random_device tRandomDevice;
+   mRandomGenerator.seed(tRandomDevice());
+   mRandomDistribution = std::uniform_int_distribution<>(mSleepMs1, mSleepMs2);
+}
+
+void RandomSleepMs::doSleep()
+{
+   // Get a random sleep time.
+   int tSleepMs = mRandomDistribution(mRandomGenerator);
+   // Sleep.
+   sleepMs(tSleepMs);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 }//namespace
 

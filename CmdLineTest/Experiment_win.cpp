3ee23f5024ef0bdd3de41103e8810ed99a09708d
chrishoen <chris123hoen@gmail.com>
2022-03-27 23:06:20 -0400

    more

--- CmdLineTest/Experiment_win.cpp
+++ CmdLineTest/Experiment_win.cpp
@@ -29,7 +29,27 @@ Experiment::Experiment()
 
 void Experiment::doRun1()
 {
-   printf("doRun1************************************************\n");
+   printf("doRun1*****************************************BEGIN\n");
+
+   while (true)
+   {
+      // Read from console input.
+      char tString[cMaxStringSize];
+      if (fgets(tString, cMaxStringSize, stdin) == 0)
+      {
+         printf("ERROR 1");
+         return;
+      }
+
+      // Remove cr/lf at end of line.
+      my_trimCRLF(tString);
+
+      if (strcmp(tString, "e") == 0) break;
+
+      printf("CMD %s\n", tString);
+   }
+
+   printf("doRun1*****************************************END\n");
 }
 
 //******************************************************************************

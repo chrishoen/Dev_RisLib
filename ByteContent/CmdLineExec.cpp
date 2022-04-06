00686ad445776e2a5c8ba0fcefdff62846f460e1
chrishoen <chris123hoen@gmail.com>
2022-04-06 18:12:17 -0400

    adding byte boundaries to byte content

--- ByteContent/CmdLineExec.cpp
+++ ByteContent/CmdLineExec.cpp
@@ -33,6 +33,70 @@ void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 //******************************************************************************
 //******************************************************************************
 
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   int tDataLen = aCmd->argInt(1);
+   int tByteBoundary = 4;
+  
+   int tOffset = tDataLen % tByteBoundary;
+   if (tOffset) tOffset = tByteBoundary - tOffset;
+   int tNewDataLen = tDataLen + tOffset;
+   Prn::print(0, "%d  %d", tDataLen, tNewDataLen);
+
+
+#if 0
+   int tNewDataLen =
+      (tDataLen + sizeof(unsigned int) - 1) & ~(sizeof(unsigned int) - 1); //For safty reasons
+   Prn::print(0, "%d  %d", tDataLen, tNewDataLen);
+#endif
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1,false);
+   bool tFlag1 = aCmd->argBool(1);
+
+   Ris::ByteBuffer tBuffer(1000);
+
+   tBuffer.setCopyTo();
+   tBuffer.copy ( &tFlag1 );
+   Prn::print(0,"Flag1          %5s",my_string_from_bool(tFlag1));
+   Prn::print(0,"Buffer.Length  %5d",tBuffer.mWorkingLength);
+
+   tBuffer.rewind();
+   tBuffer.setCopyFrom();
+
+   tFlag1 = false;
+   tBuffer.copy ( &tFlag1 );
+   Prn::print(0,"Flag1          %5s",my_string_from_bool(tFlag1));
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+
+void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
 void CmdLineExec::executeGo6(Ris::CmdLineCmd* aCmd)
 {
    Ris::ByteBuffer tBuffer(20000);
@@ -108,71 +172,5 @@ void CmdLineExec::executeGo7(Ris::CmdLineCmd* aCmd)
 void CmdLineExec::executeGo8(Ris::CmdLineCmd* aCmd)
 {
    Ris::ByteBuffer tBuffer(20000);
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-
-void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
-{
-   aCmd->setArgDefault(1,1001);
-   int tCount1 = aCmd->argInt(1);
-   int tCount2 = 0;
-
-   Ris::ByteBuffer tBuffer(1000);
-
-   tBuffer.setCopyTo();
-   tBuffer.copy ( &tCount1 );
-   Prn::print(0,"Buffer.Length %d",tBuffer.mWorkingLength);
-
-   tBuffer.rewind();
-   tBuffer.setCopyFrom();
-
-   tCount1=0;
-   tBuffer.copy ( &tCount1 );
-   Prn::print(0,"Count1        %d",tCount1);
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-
-void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
-{
-   aCmd->setArgDefault(1,false);
-   bool tFlag1 = aCmd->argBool(1);
-
-   Ris::ByteBuffer tBuffer(1000);
-
-   tBuffer.setCopyTo();
-   tBuffer.copy ( &tFlag1 );
-   Prn::print(0,"Flag1          %5s",my_string_from_bool(tFlag1));
-   Prn::print(0,"Buffer.Length  %5d",tBuffer.mWorkingLength);
-
-   tBuffer.rewind();
-   tBuffer.setCopyFrom();
-
-   tFlag1 = false;
-   tBuffer.copy ( &tFlag1 );
-   Prn::print(0,"Flag1          %5s",my_string_from_bool(tFlag1));
-}
-
-//******************************************************************************
-
-void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
-{
-}
-
-//******************************************************************************
-
-void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
-{
-}
-
-//******************************************************************************
-
-void CmdLineExec::executeGo5(Ris::CmdLineCmd* aCmd)
-{
 }
 

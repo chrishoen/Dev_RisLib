0c0cd259880fc6fcc27cda9dc10f9e784145f84c
chrishoen <chris123hoen@gmail.com>
2022-05-11 22:05:36 -0400

    more

--- Script/MainInit.cpp
+++ Script/MainInit.cpp
@@ -2,6 +2,7 @@
 
 
 #include "risThreadsProcess.h"
+#include "risBaseDir.h"
 
 //******************************************************************************
 //******************************************************************************
@@ -10,6 +11,52 @@
 
 void main_initialize(int argc,char** argv)
 {
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Process configuration.
+
+   // Set the program current working directory up one level from the 
+   // program bin directory.
+   Ris::portableChdirUpFromBin();
+
+   // Set the base directory to the current working directory.
+   Ris::setBaseDirectoryToCurrent();
+
+   // Set the process priority class.
+   Ris::Threads::enterProcessHigh();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize print facility.
+
+   // Initialize print.
+   Prn::resetPrint();
+   Prn::initializePrint();
+
+   // Initialize print filters.
+   Prn::setFilter(Prn::Show1, true);
+   Prn::setFilter(Prn::Show2, false);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize trace facility.
+
+   Trc::reset();
+   Trc::create_buffer(1, 3, "message");
+   Trc::create_buffer(11, 3, "serial");
+   Trc::set_default_trace_index(11);
+   //Trc::create_log(11, 4, "log/ProtoSerial_trace11.log");
+   Trc::initialize();
+
+
+
+
+
+
+
    //***************************************************************************
    //***************************************************************************
    //***************************************************************************

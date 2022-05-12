9d156c08a55a2c36eb311091fdbd7d5e172d9654
chrishoen <chris123hoen@gmail.com>
2022-05-11 21:18:03 -0400

    more

---
+++ Script/ScriptReader.cpp
@@ -0,0 +1,58 @@
+#include "stdafx.h"
+
+#include "ScriptReader.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+ScriptReader::ScriptReader()
+{
+}
+
+void ScriptReader::reset()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Execute a command line command. It calls one of
+// the following specific command execution functions.
+
+void ScriptReader::execute(Ris::CmdLineCmd* aCmd)
+{
+   if(aCmd->isCmd("RESET"))   reset();
+   if (aCmd->isCmd("RED"))    executeRed(aCmd);
+   if (aCmd->isCmd("GREEN"))  executeGreen(aCmd);
+   if (aCmd->isCmd("BLACK"))  executeBlack(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void ScriptReader::executeRed(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 111);
+   int tN = aCmd->argInt(1);
+   Prn::print(0, "RED     %d", tN);
+}
+
+void ScriptReader::executeGreen(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 111);
+   int tN = aCmd->argInt(1);
+   Prn::print(0, "GREEN   %d", tN);
+}
+
+void ScriptReader::executeBlack(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 111);
+   int tN = aCmd->argInt(1);
+   Prn::print(0, "BLACK   %d", tN);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************

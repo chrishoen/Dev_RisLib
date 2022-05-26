b7a86048b5d61b8262b4795bcad9292cdcb38c55
chrishoen <chris123hoen@gmail.com>
2022-05-26 16:06:32 -0400

    added ProtoUdpTest

---
+++ ProtoUdpTest/CmdLineExec.cpp
@@ -0,0 +1,165 @@
+
+#include "stdafx.h"
+
+#include "procoUdpSettings.h"
+#include "procoMsg.h"
+#include "procoMsgHelper.h"
+
+#include "procoPeerThread.h"
+
+#include "CmdLineExec.h"
+
+using namespace ProtoComm;
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+CmdLineExec::CmdLineExec()
+{
+}
+void CmdLineExec::reset()
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
+{
+   if (aCmd->isCmd("TP"))        ProtoComm::gPeerThread->mTPCode = aCmd->argInt(1);
+   if (aCmd->isCmd("SEND"))      executeSend(aCmd);
+   if (aCmd->isCmd("ECHO"))      executeEcho(aCmd);
+   if (aCmd->isCmd("DATA"))      executeData(aCmd);
+   if (aCmd->isCmd("GO1"))       executeGo1(aCmd);
+   if (aCmd->isCmd("GO2"))       executeGo2(aCmd);
+   if (aCmd->isCmd("GO3"))       executeGo3(aCmd);
+   if (aCmd->isCmd("GO4"))       executeGo4(aCmd);
+   if (aCmd->isCmd("Parms"))     executeParms(aCmd);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::special(int aSpecial)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeSend (Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1,1);
+   int tMsgType= aCmd->argInt(1);
+
+   switch (tMsgType)
+   {
+      case 1:
+      {
+         ProtoComm::TestMsg* tMsg = new ProtoComm::TestMsg;
+         MsgHelper::initialize(tMsg);
+         gPeerThread->sendMsg(tMsg);
+         break;
+      }
+      case 5:
+      {
+         ProtoComm::DataMsg* tMsg = new ProtoComm::DataMsg;
+         MsgHelper::initialize(tMsg);
+         gPeerThread->sendMsg(tMsg);
+         break;
+      }
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeEcho(Ris::CmdLineCmd* aCmd)
+{
+   aCmd->setArgDefault(1, 0);
+   int tNumWords = aCmd->argInt(1);
+   
+   ProtoComm::EchoRequestMsg* tMsg = new ProtoComm::EchoRequestMsg;
+   MsgHelper::initialize(tMsg,tNumWords);
+   gPeerThread->sendMsg(tMsg);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeData(Ris::CmdLineCmd* aCmd)
+{
+   ProtoComm::DataMsg* tMsg = new ProtoComm::DataMsg;
+   MsgHelper::initialize(tMsg);
+
+   gPeerThread->sendMsg(tMsg);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void test1(Ris::ByteContent* aMsg)
+{
+   ProtoComm::BaseMsg* tMsg = (ProtoComm::BaseMsg*)aMsg;
+   Prn::print(0, "MessageType %d", tMsg->mMessageType);
+}
+
+void test2(Ris::ByteContent* aMsg)
+{
+   test1(aMsg);
+}
+
+void CmdLineExec::executeGo1(Ris::CmdLineCmd* aCmd)
+{
+   ProtoComm::TestMsg* tMsg = new ProtoComm::TestMsg;
+   test1(tMsg);
+   test2(tMsg);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo2(Ris::CmdLineCmd* aCmd)
+{
+   ProtoComm::MsgMetrics tMsgMetrics;
+   ProtoComm::TestMsg* tMsg = new ProtoComm::TestMsg;
+   tMsgMetrics.update(tMsg, 100);
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo3(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeGo4(Ris::CmdLineCmd* aCmd)
+{
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
+void CmdLineExec::executeParms(Ris::CmdLineCmd* aCmd)
+{
+   ProtoComm::gUdpSettings.show();
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+
---
+++ ProtoUdpTest/MainInit.cpp
@@ -0,0 +1,89 @@
+
+#include "stdafx.h"
+
+#include "risThreadsProcess.h"
+#include "risBaseDir.h"
+#include "procoUdpSettings.h"
+#include "MainInit.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize.
+
+void main_initialize()
+{
+   printf("ProtoUdpTest Program**********************************************BEGIN\n");
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Process configuration.
+
+   // Set the program current working directory up one level from the 
+   // program bin directory.
+   Ris::portableChdirUpFromBin();
+
+   // Set the base directory to the current working directory.
+   Ris::setBaseDirectoryToCurrent();
+
+   // Set the process priority class.
+   Ris::Threads::enterProcessHigh();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize print facility.
+
+   // Initialize print.
+   Prn::resetPrint();
+   Prn::initializePrint();
+
+   // Initialize print filters.
+   Prn::setFilter(Prn::Show1, true);
+   Prn::setFilter(Prn::Show2, false);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize trace facility.
+
+   Trc::reset();
+   Trc::create_buffer(1,  3, "message");
+   Trc::create_buffer(11, 3, "udp");
+   Trc::set_default_trace_index(11);
+   Trc::create_log(11, 4, "log/ProtoUdpTest_trace11.log");
+   Trc::initialize();
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Read parameters files.
+
+   ProtoComm::gUdpSettings.reset();
+   ProtoComm::gUdpSettings.readSection("default");
+   ProtoComm::gUdpSettings.readSection("UdpPeer1");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize.
+
+void main_finalize()
+{
+   // Finalize print facility.
+   Prn::finalizePrint();
+
+   // Finalize trace facility.
+   Trc::finalize();
+
+   // Exit process
+   Ris::Threads::exitProcess();
+
+   printf("ProtoUdpTest Program**********************************************BEGIN\n");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************

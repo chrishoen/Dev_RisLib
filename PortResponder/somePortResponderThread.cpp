977326df49712f80a2643a2a3c9b3b5e74120835
chrishoen <chris123hoen@gmail.com>
2022-06-13 17:20:22 -0400

    no separate udp thread

--- PortResponder/somePortResponderThread.cpp
+++ PortResponder/somePortResponderThread.cpp
@@ -6,8 +6,14 @@
 //******************************************************************************
 #include "stdafx.h"
 
+#include "someSerialParms.h"
+
 #define  _SOMEPORTRESPONDERTHREAD_CPP_
 #include "somePortResponderThread.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
 
 namespace Some
 {
@@ -16,126 +22,98 @@ namespace Some
 //******************************************************************************
 //******************************************************************************
 // Constructor.
+
 PortResponderThread::PortResponderThread()
 {
-   // Set base class variables.
+   // Set base class thread services.
    BaseClass::setThreadName("Serial");
    BaseClass::setThreadPriorityHigh();
-   BaseClass::mTimerPeriod = 1000;
 
-   // Initialize qcalls.
-   mRxStringQCall.bind   (this,&PortResponderThread::executeRxString);
+   // Set base class thread priority.
+   BaseClass::setThreadPriorityHigh();
 
-   // Initialize member variables.
-   mUdpStringThread = 0;
-   mConnectionFlag = false;
-   mTPFlag = false;
-}
-
-PortResponderThread::~PortResponderThread()
-{
-   if (mUdpStringThread) delete mUdpStringThread;
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Place holder.
-
-void PortResponderThread::show()
-{
+   // Initialize variables.
+   mRxBuffer[0] = 0;
+   mTxBuffer[0] = 0;
+   mErrorCount = 0;
+   mRestartCount = 0;
+   mRxCount = 0;
+   mTxCount = 0;
+   mRxReqNumBytes = gSerialParms.mRxReqNumBytes;
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Thread init function. This is called by the base class immedidately 
-// after the thread starts running. It creates and launches the 
-// child UdpStringThread.
+// Thread init function. This is called by the base class immediately
+// after the thread starts running. It initializes the serial port.
 
 void PortResponderThread::threadInitFunction()
 {
+   Trc::write(11, 0, "PortResponderThread::threadInitFunction");
+
    // Instance of network socket settings.
    Ris::Net::Settings tSettings;
-   int tPort = 5012;
-   tSettings.setLocalPort(tPort);
-// tSettings.setRemoteAddress("192.168.1.134", tPort);
-   tSettings.setUdpWrapFlag(true);
-   tSettings.mUdpWrapFlag = true;
-   tSettings.mRxStringQCall = mRxStringQCall;
-   tSettings.mTraceIndex = 11;
-   Trc::start(11);
 
-   // Create child thread with the settings.
-   mUdpStringThread = new Ris::Net::UdpStringThread(tSettings);
-
-   // Launch child thread.
-   mUdpStringThread->launchThread(); 
+   mRxSocket.configureLocal(5012);
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Thread exit function. This is called by the base class immedidately
-// before the thread is terminated. It shuts down the child UdpStringThread.
+// Thread run function. This is called by the base class immediately
+// after the thread init function. It runs a loop that blocks on 
+// serial port receives and then processes them. The loop terminates
+// when the serial port receive is aborted.
+
+void PortResponderThread::threadRunFunction()
+{
+   while (mRxSocket.doRecvString())
+   {
+      puts(mRxSocket.mRxString);
+   }
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Thread exit function. This is called by the base class immediately
+// before the thread is terminated. It is close the serial port.
 
 void PortResponderThread::threadExitFunction()
 {
-   Prn::print(0, "PortResponderThread::threadExitFunction BEGIN");
+   printf("somePortResponderThread::threadExitFunction\n");
 
-   // Shutdown the child thread.
-   mUdpStringThread->shutdownThread();
-
-   Prn::print(0, "PortResponderThread::threadExitFunction END");
+   // Close the serial port.
+   mRxSocket.doClose();
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Thread shutdown function. This calls the base class shutdownThread
-// function to terminate the thread. This executes in the context of
-// the calling thread.
+// Thread shutdown function. This is called out of the context of
+// this thread. It aborts the serial port receive and waits for the
+// thread to terminate after execution of the thread exit function.
 
 void PortResponderThread::shutdownThread()
 {
-   Prn::print(0, "PortResponderThread::shutdownThread BEGIN");
+   printf("somePortResponderThread::shutdownThread\n");
+
+   // Abort pending serial port receives
+   mRxSocket.doClose();
+
+   // Wait for thread to terminate.
    BaseClass::shutdownThread();
-   Prn::print(0, "PortResponderThread::shutdownThread END");
 }
 
 //******************************************************************************
 //******************************************************************************
 //******************************************************************************
-// Execute periodically. This is called by the base class timer.
+// Send bytes via the serial port. This executes in the context of
+// the calling thread.
 
-void PortResponderThread::executeOnTimer(int aTimerCount)
+void PortResponderThread::sendString(char* aString)
 {
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// qcall registered to the mUdpStringThread child thread. It is invoked by
-// the child thread when a string is received.
-// Based on the receive string type, call one of the specific receive
-// string handlers. This is bound to the qcall.
-
-void PortResponderThread::executeRxString(std::string* aString)
-{
-   Prn::print(Prn::Show1, "PortResponderThread::executeRxString %s", aString->c_str());
-   delete aString;
-
-   sendString(new std::string("Mira Hello"));
-}
-
-//******************************************************************************
-//******************************************************************************
-//******************************************************************************
-// Send a string via mUdpStringThread:
-
-void PortResponderThread::sendString(std::string* aString)
-{
-   mUdpStringThread->sendString(aString);
 }
 
 //******************************************************************************

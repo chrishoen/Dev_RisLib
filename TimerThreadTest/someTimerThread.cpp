9f195acbff829865ef239eff5315f6f16183e4ef
chrishoen <chris123hoen@gmail.com>
2022-06-15 14:02:18 -0400

    working on timer thread test

--- TimerThreadTest/someTimerThread.cpp
+++ TimerThreadTest/someTimerThread.cpp
@@ -32,11 +32,12 @@ TimerThread::TimerThread()
 
    // Set timer period.
    BaseClass::mTimerPeriod = 250;
+   BaseClass::mTimerPeriod = 1000;
    mTimeMarker.initialize(100, 250*1000);
 
    // Members
    mTPFlag = false;
-   mTestCode = 1;
+   mTestCode = 2;
    mTestCount = 0;
 }
 
@@ -46,9 +47,7 @@ TimerThread::TimerThread()
 
 void TimerThread::threadInitFunction()
 {
-   if (mTestCode !=3) return;
-
-   BaseClass::showThreadFullInfo();
+   Prn::print(0, "TimerThread::threadInitFunction");
 }
 
 //******************************************************************************
@@ -57,13 +56,10 @@ void TimerThread::threadInitFunction()
 
 void TimerThread::executeOnTimer(int aTimeCount)
 {
-   if (aTimeCount < 20) return;
-
-   switch (mTestCode)
-   {
-      case 1: executeTest1(aTimeCount); break;
-      case 2: executeTest2(aTimeCount); break;
-   }
+   Prn::print(0, "TEST2 %5d $$ %3d %10.4f",
+      aTimeCount,
+      BaseClass::getThreadProcessorNumber(),
+      Ris::getProgramTime());
 }
 
 //******************************************************************************

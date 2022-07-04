116d1643125d93faf2292d62474e1a666b1e2e9b
chrishoen <chris123hoen@gmail.com>
2022-07-03 20:51:05 -0400

    more

--- ThreadSynch/CmdLineExec.cpp
+++ ThreadSynch/CmdLineExec.cpp
@@ -31,6 +31,7 @@ void CmdLineExec::reset()
 
 void CmdLineExec::execute(Ris::CmdLineCmd* aCmd)
 {
+   if (aCmd->isCmd("TP"))     Some::gThread1->mTPFlag = aCmd->argBool(1);
    if (aCmd->isCmd("P"))      executePut(aCmd);
    if (aCmd->isCmd("GO2"))    executeGo2(aCmd);
    if (aCmd->isCmd("GO3"))    executeGo3(aCmd);

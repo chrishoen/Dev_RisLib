1fa501a2f2a2f1136aa22f72ebb41624c39a3a0a
chrishoen <chris123hoen@gmail.com>
2022-11-06 09:05:16 -0500

    new install cmake files

---
+++ MyInstall.cmake
@@ -0,0 +1,14 @@
+﻿cmake_minimum_required(VERSION 3.10)
+
+if (MSVC)
+   install(TARGETS ${project} DESTINATION "C:/aaa_prime/RisLib")
+elseif(MYMODE STREQUAL "prime-x64")
+   install(TARGETS ${project} DESTINATION "/opt/prime/bin")
+elseif (MYMODE STREQUAL "prime-arm7")
+   install(CODE "execute_process(COMMAND_ECHO STDOUT
+      COMMAND c:/windows/system32/openssh/scp ${CMAKE_CURRENT_BINARY_DIR}/${project} debian@bbx:/opt/prime/bin)")
+elseif (MYMODE STREQUAL "prime-arm8")
+   install(CODE "execute_process(COMMAND_ECHO STDOUT
+      COMMAND scp ${CMAKE_CURRENT_BINARY_DIR}/${project} stenotgt:/opt/prime/bin)")
+endif()
+

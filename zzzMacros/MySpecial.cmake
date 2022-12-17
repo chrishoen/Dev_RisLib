f50a9c7140b8fb41959ab9f98b5135ca65ccbc18
chrishoen <chris123hoen@gmail.com>
2022-12-17 13:28:01 -0500

    adding MYSTANDALONE for only build rislib

--- zzzMacros/MySpecial.cmake
+++ zzzMacros/MySpecial.cmake
@@ -27,6 +27,7 @@ function(my_print_cmake_vars)
    message(STATUS "PROJECT_SOURCE_DIR       " ${PROJECT_SOURCE_DIR})
    message(STATUS "THIRD_PARTY_LIB_DIR      " ${THIRD_PARTY_LIB_DIR})
    message(STATUS "MYMODE                   " ${MYMODE})
+   message(STATUS "MYSTANDALONE             " ${MYSTANDALONE})
    message(STATUS "my_special1***********************************************************END")
 endfunction()
 

eb7a6094e844ddc5924be3b3ef2c31d147581db4
chrishoen <chris123hoen@gmail.com>
2023-03-18 15:50:54 -0400

    cmake install stuff

--- zzzMacros/MyInstall.cmake
+++ zzzMacros/MyInstall.cmake
@@ -1,7 +1,8 @@
 ﻿cmake_minimum_required(VERSION 3.10)
 
 if (MSVC)
-   install(TARGETS ${project} DESTINATION "C:/aaa_prime/RisLib")
+#  install(TARGETS ${project} DESTINATION "C:/aaa_prime/RisLib")
+   install(TARGETS ${project} DESTINATION "C:/ProgramData/Ndisprot/bin")
 elseif(MYMODE STREQUAL "prime-ubu")
 #  install(TARGETS ${project} DESTINATION "/opt/prime/bin")
    install(CODE "execute_process(COMMAND_ECHO STDOUT

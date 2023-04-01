0355f46b65b5898f40cc8a75affc76593d2ac019
chrishoen <chris123hoen@gmail.com>
2023-04-01 08:44:55 -0400

    reworking compiler options

--- zzzMacros/MyCodeMacros.cmake
+++ zzzMacros/MyCodeMacros.cmake
@@ -4,7 +4,7 @@
 
 function(my_find_src_files _a_src_files _target)
 
-   file(GLOB _src_files RELATIVE ${PROJECT_SOURCE_DIR} *.cpp)
+   file(GLOB _src_files RELATIVE ${PROJECT_SOURCE_DIR} *.cpp *.c)
 
    if(MSVC)
       list(FILTER _src_files EXCLUDE REGEX ".*_linux.cpp$")
@@ -14,8 +14,9 @@ function(my_find_src_files _a_src_files _target)
 
    set(${_a_src_files} ${_src_files} PARENT_SCOPE)
 
-   message(STATUS "my_find_src_files******************************" ${_target})
+   message(STATUS "my_find_src_files************************ " ${_target})
    return()
+   message(STATUS "***********************************************************" ${_target})
    message(STATUS "my_find_src_files************************BEGIN " ${_target})
    foreach(_src_file ${_src_files})
       message(STATUS ${_src_file})
@@ -38,48 +39,93 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_add_compile_options _target)
+#*******************************************
+function(my_add_compile_options_c_gcc option)
+   add_compile_options($<$<COMPILE_LANG_AND_ID:C,GNU>:${option}>)
+endfunction()
+function(my_add_compile_options_cxx_gcc option)
+   add_compile_options($<$<COMPILE_LANG_AND_ID:CXX,GNU>:${option}>)
+endfunction()
 
+#*******************************************
+function(my_append_compile_flags_gcc)
+
+    my_add_compile_options_c_gcc(-O3)
+    my_add_compile_options_c_gcc(-w)
+    my_add_compile_options_c_gcc(-fno-strict-aliasing)
+    #my_add_compile_options_c_gcc(-pipe)
+    #my_add_compile_options_c_gcc(-feliminate-unused-debug-types)
+    #my_add_compile_options_c_gcc(-fno-omit-frame-pointer)
+    #my_add_compile_options_c_gcc(-mcpu=cortex-a53+crc+crypto)
+    #my_add_compile_options_c_gcc(-D_FORTIFY_SOURCE=2)
+    #my_add_compile_options_c_gcc(--sysroot=${CMAKE_SYSROOT})
+
+    my_add_compile_options_cxx_gcc(-O3)
+    my_add_compile_options_cxx_gcc(-Wall)
+    my_add_compile_options_cxx_gcc(-Wextra)
+    my_add_compile_options_cxx_gcc(-Wno-unused-parameter)
+    my_add_compile_options_cxx_gcc(-Wno-unused-result)
+    my_add_compile_options_cxx_gcc(-Wno-unused-variable)
+    my_add_compile_options_cxx_gcc(-Wno-unused-function)
+    my_add_compile_options_cxx_gcc(-Wno-unknown-pragmas)
+    my_add_compile_options_cxx_gcc(-Wno-missing-field-initializers)
+    my_add_compile_options_cxx_gcc(-Wno-delete-incomplete)
+    my_add_compile_options_cxx_gcc(-fno-strict-aliasing)
+    my_add_compile_options_cxx_gcc(-Wno-psabi)
+    #my_add_compile_options_cxx_gcc(-pipe)
+    #my_add_compile_options_cxx_gcc(-feliminate-unused-debug-types)
+    #my_add_compile_options_cxx_gcc(-fno-omit-frame-pointer)
+    #my_add_compile_options_cxx_gcc(-mcpu=cortex-a53+crc+crypto)
+    #my_add_compile_options_cxx_gcc(-D_FORTIFY_SOURCE=2)
+    #my_add_compile_options_cxx_gcc(--sysroot=${CMAKE_SYSROOT})
+endfunction()
+
+#*******************************************
+function(my_append_compile_flags_clang)
+
+    add_compile_options(-Wno-deprecated-volatile)
+    add_compile_options(-Wno-delete-incomplete)
+
+endfunction()
+
+#*******************************************
+function(my_append_compile_flags_msvc)
+
+    add_compile_options(/WX)
+    add_compile_options(/wd4996)
+    add_compile_options(/wd6031)
+    add_compile_options(/wd6385)
+    add_compile_options(/O2)
+    add_compile_options(/Ot)
+    add_compile_options(/Oi)
+    add_compile_options(/MT)
+    add_compile_options(/GS-)
+    add_compile_options(/Gd)
+    add_compile_options(/EHsc)
+    add_compile_options(/MT)
+    add_compile_options(/Zc:wchar_t)
+    add_compile_options(/Zc:inline)
+    add_compile_options(/D_MBCS)
+
+endfunction()
+
+#*******************************************
+function(my_append_compile_flags)
    if(MSVC)
-      target_compile_options(${_target} PRIVATE "/WX")
-      target_compile_options(${_target} PRIVATE "/wd4996")
-      target_compile_options(${_target} PRIVATE "/wd6031")
-      target_compile_options(${_target} PRIVATE "/wd6385")
-
-      target_compile_options(${_target} PRIVATE "/O2")
-      target_compile_options(${_target} PRIVATE "/Ot")
-      target_compile_options(${_target} PRIVATE "/Oi")
-
-      target_compile_options(${_target} PRIVATE "/MT")
-      target_compile_options(${_target} PRIVATE "/GS-")
-      target_compile_options(${_target} PRIVATE "/Gd")
-
-      target_compile_options(${_target} PRIVATE "/EHsc")
-      target_compile_options(${_target} PRIVATE "/MT")
-      target_compile_options(${_target} PRIVATE "/Zc:wchar_t")
-      target_compile_options(${_target} PRIVATE "/Zc:inline")
-
-      target_compile_options(${_target} PRIVATE "/D_MBCS")
-
+      my_append_compile_flags_msvc()
    elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-      target_compile_options(${_target} PRIVATE "-std=c++11")
-      target_compile_options(${_target} PRIVATE "-fexceptions")
-      target_compile_options(${_target} PRIVATE "-O3")
-      target_compile_options(${_target} PRIVATE "-fthreadsafe-statics")
-      target_compile_options(${_target} PRIVATE "-frtti")
-      target_compile_options(${_target} PRIVATE "-Wno-unused-result")
-      target_compile_options(${_target} PRIVATE "-Wno-stringop-overflow")
-      target_compile_options(${_target} PRIVATE "-Wno-deprecated-declarations")
-      target_compile_options(${_target} PRIVATE "-Wno-delete-incomplete")
-      target_compile_options(${_target} PRIVATE "-Wno-format")
-      target_compile_options(${_target} PRIVATE "-Wno-write-strings")
-      target_compile_options(${_target} PRIVATE "-Wno-psabi")
-      target_compile_definitions(${_target} PRIVATE "-DNDEBUG")
-
+      my_append_compile_flags_gcc()
    elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-      target_compile_options(${_target} PRIVATE "-w")
+      my_append_compile_flags_clang()
    endif()
+endfunction()
 
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_add_compile_options _target)
+   return()
 endfunction()
 
 #*******************************************************************************
@@ -87,58 +133,8 @@ endfunction()
 #*******************************************************************************
 
 function(my_add_compile_options_so _target)
-
-   message(STATUS "my_add_compile_options************************ " ${_target})
-
-   if(MSVC)
-      target_compile_options(${_target} PRIVATE "/WX")
-      target_compile_options(${_target} PRIVATE "/wd4996")
-      target_compile_options(${_target} PRIVATE "/wd6031")
-      target_compile_options(${_target} PRIVATE "/wd6385")
-
-      target_compile_options(${_target} PRIVATE "/O2")
-      target_compile_options(${_target} PRIVATE "/Ot")
-      target_compile_options(${_target} PRIVATE "/Oi")
-
-      target_compile_options(${_target} PRIVATE "/MT")
-      target_compile_options(${_target} PRIVATE "/GS-")
-      target_compile_options(${_target} PRIVATE "/Gd")
-
-      target_compile_options(${_target} PRIVATE "/EHsc")
-      target_compile_options(${_target} PRIVATE "/MT")
-      target_compile_options(${_target} PRIVATE "/Zc:wchar_t")
-      target_compile_options(${_target} PRIVATE "/Zc:inline")
-
-      target_compile_options(${_target} PRIVATE "/D_MBCS")
-
-   elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-      target_compile_options(${_target} PRIVATE "-std=c++11")
-      target_compile_options(${_target} PRIVATE "-fexceptions")
-      target_compile_options(${_target} PRIVATE "-O3")
-      target_compile_options(${_target} PRIVATE "-fthreadsafe-statics")
-      target_compile_options(${_target} PRIVATE "-frtti")
-      target_compile_options(${_target} PRIVATE "-fPIC")
-      target_compile_options(${_target} PRIVATE "-Wno-stringop-overflow")
-      target_compile_options(${_target} PRIVATE "-Wno-deprecated-declarations")
-      target_compile_options(${_target} PRIVATE "-Wno-delete-incomplete")
-      target_compile_options(${_target} PRIVATE "-Wno-format")
-      target_compile_options(${_target} PRIVATE "-Wno-write-strings")
-      target_compile_options(${_target} PRIVATE "-Wno-psabi")
-      target_compile_definitions(${_target} PRIVATE "-DNDEBUG")
-
-   elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-      target_compile_options(${_target} PRIVATE "-w")
-   endif()
-
+   return()
 endfunction()
-
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
-
 
 #*******************************************************************************
 #*******************************************************************************

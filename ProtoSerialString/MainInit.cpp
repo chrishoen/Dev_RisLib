9e7fa971dffeace811370257dd7789f5263ddf9e
chrishoen <chris123hoen@gmail.com>
2023-04-29 09:23:19 -0400

    prints

--- ProtoSerialString/MainInit.cpp
+++ ProtoSerialString/MainInit.cpp
@@ -43,7 +43,7 @@ void main_initialize(int argc,char** argv)
    Prn::initializePrint();
 
    // Initialize print filters.
-   Prn::setFilter(Prn::Show1, false);
+   Prn::setFilter(Prn::Show1, true);
    Prn::setFilter(Prn::Show2, false);
 
    //***************************************************************************

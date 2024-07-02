7049217e7c8e1f850f6656bd2f55f23c095be2fb
chrishoen <chris123hoen@gmail.com>
2024-07-02 16:24:40 -0400

    more

--- RisLib/risSockets_win.cpp
+++ RisLib/risSockets_win.cpp
@@ -355,7 +355,8 @@ bool BaseSocket::ioctlBlocking(bool aBlocking)
 bool BaseSocket::ioctlFlush ()
 {
    int tStatus = 0;
-   tStatus = ioctlsocket(mBaseSpecific->mDesc,SIO_FLUSH,(u_long*)0);
+   u_long value = 0;
+   tStatus = ioctlsocket(mBaseSpecific->mDesc,SIO_FLUSH,&value);
    return updateError(tStatus);
 }
 
